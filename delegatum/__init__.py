afae22a1b0b9f800e4999503e1bfffe83c25149c
Brendan Strejcek <brendan@datagazing.com>
2024-01-03 12:42:51 -0500

    Bump version: 0.1.0 → 0.2.0

--- delegatum/__init__.py
+++ delegatum/__init__.py
@@ -55,6 +55,6 @@ External modules:
 
 __author__ = """Brendan Strejcek"""
 __email__ = 'brendan@datagazing.com'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 from .delegatum import Delegatum # noqa F401
